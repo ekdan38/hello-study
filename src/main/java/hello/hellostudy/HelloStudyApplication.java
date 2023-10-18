ecb8715064ebe16f507b966c39d61e8dbaa31efd
HongJeongHwa <spie99@naver.com>
2023-10-18 20:44:47 +0900

    spring day - 1 commit

---
+++ src/main/java/hello/hellostudy/HelloStudyApplication.java
@@ -0,0 +1,13 @@
+package hello.hellostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellostudy/HelloStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
