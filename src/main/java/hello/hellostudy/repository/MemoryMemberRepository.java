b14054b4464eb0e009972d29695e99c4344d2626
HongJeongHwa <spie99@naver.com>
2023-11-20 22:59:41 +0900

    Member.java => 회원 객체 MemberRepository => 회원 리포지토리 인터페이스 MemoryMemberRepository.java => 회원 리포지토리 메모리 구현체 MemoryMemberRepositoryTest.java => 회원 리포지토리 테스트 케이스 작성.
    결과 : 회원 도메인 생성, 회원 저장소 생성, 테스트 작성.

--- src/main/java/hello/hellostudy/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellostudy/repository/MemoryMemberRepository.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
 @Repository
+
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
     @Override
+
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
