057fae8a37359741c7496292608de29b7396c4df
HongJeongHwa <spie99@naver.com>
2023-11-28 22:34:39 +0900

    Member.java => 회원 객체 MemberRepository => 회원 리포지토리 인터페이스 MemoryMemberRepository.java => 회원 리포지토리 메모리 구현체 MemoryMemberRepositoryTest.java => 회원 리포지토리 테스트 케이스 작성.
    결과 : 회원 도메인 생성, 회원 저장소 생성, 테스트 작성.

---
+++ src/main/java/hello/hellostudy/SpringConfig.java
@@ -0,0 +1,23 @@
+package hello.hellostudy;
+
+import hello.hellostudy.domain.Member;
+import hello.hellostudy.repository.MemberRepository;
+import hello.hellostudy.repository.MemoryMemberRepository;
+import hello.hellostudy.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(MemberRepository);
+    }
+
+    @Bean
+    public MemoryMemberRepository memoryMemberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+}
+
