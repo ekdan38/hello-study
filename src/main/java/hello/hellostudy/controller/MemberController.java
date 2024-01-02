882068ee3badf941e341696fc2306eef6daa39ee
HongJeongHwa <spie99@naver.com>
2024-01-02 12:13:37 +0900

    Member.java => 회원 객체 MemberRepository => 회원 리포지토리 인터페이스 MemoryMemberRepository.java => 회원 리포지토리 메모리 구현체 MemoryMemberRepositoryTest.java => 회원 리포지토리 테스트 케이스 작성.
    결과 : 회원 도메인 생성, 회원 저장소 생성, 테스트 작성.

--- src/main/java/hello/hellostudy/controller/MemberController.java
+++ src/main/java/hello/hellostudy/controller/MemberController.java
@@ -9,11 +9,11 @@ public class MemberController {
     /* private final MemberService memberService = new MemberService();
        MemberService은 현재 별 기능이 없다 그래서 스프링 컨테이너에 등록을 하고 사용한다.
        메서드를 호출할때마다 새로운 객체를 생성하는 new는 새로운 인스턴스를 생성하기때문에 사용하는 시점마다
-       메모리를 추가로 할당받는다. 따라서 new를 사용하면 객체 생성에 비용이 많이 들 수 있다.
+       메모리를 추가로 할당받는다. 따라서 new를 사                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     용하면 객체 생성에 비용이 많이 들 수 있다.
        이에 반해 DI(Dependency Injection)는 객체를 외부에서 생성해서 주입받는 방식이다.
        필요할 경우에만 객체를 생상하고 재사용 가능.
 
-       따라서, DI를 사용하는 방법으로 객체를 생성하고 의존성을 주입받는 것이 보다 효율적인 방법이다.
+       따라서, DI를 사용하는 방법으로 객체를 생성하고  의존성을 주입받는 것이 보다 효율적인 방법이다.
      */
     private final MemberService memberService;
 
