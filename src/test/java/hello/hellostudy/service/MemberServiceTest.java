cdc6b66e4f5491fb4499da724fdbf1957d4b73c0
HongJeongHwa <spie99@naver.com>
2024-02-04 21:25:14 +0900

    Member.java => 회원 객체 MemberRepository => 회원 리포지토리 인터페이스 MemoryMemberRepository.java => 회원 리포지토리 메모리 구현체 MemoryMemberRepositoryTest.java => 회원 리포지토리 테스트 케이스 작성.
    결과 : 회원 도메인 생성, 회원 저장소 생성, 테스트 작성.
    
    24.01.20

--- src/test/java/hello/hellostudy/service/MemberServiceTest.java
+++ src/test/java/hello/hellostudy/service/MemberServiceTest.java
@@ -14,6 +14,7 @@ class MemberServiceTest {
     MemberService memberService;
     MemoryMemberRepository memberRepository;
 
+
     @BeforeEach
     public void beforeEach(){
         memberRepository = new MemoryMemberRepository();
